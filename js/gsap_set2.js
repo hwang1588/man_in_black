408cc7b034eda8a2d392bdcc56d82cdd809b80e0
hwang1588 <75598328+hwang1588@users.noreply.github.com>
2023-06-09 15:37:49 +0900

    -

--- js/gsap_set2.js
+++ js/gsap_set2.js
@@ -1,7 +1,5 @@
 
-$(window).on('load', function() {
-  $('.bgm').get(0).play();
-});
+
 ///////////////////////////////////////////////////
 //timeline.to 에서 to를 통해 해당 요소에 명령 입력
 ///////////////////////////////////////////////////
@@ -40,16 +38,22 @@ gsap.to("#pintest", {
       start: "center center", //요소 트리거위치, 뷰포트 트리거 위치
       end: "+=500", //트리거가 끝날때까지의 길이(px)
       // markers: true,
+      onEnter: () => {
+        $('.hand_sound')[0].play();
+      },  
+      onLeave: () => {
+        $('.hand_sound')[0].pause();
+      },  
     },
   });
   gsap.to(".hand_2", {
     opacity: 1,
     scrollTrigger: {
-      trigger: "#pintest4", //트리거용 요소
+      trigger: "pintest4", //트리거용 요소
       pin: "#pintest4", //true,false로 핀 유무 결정, 요소로 입력시 해당 요소가 고정됨
       start: "center center", //요소 트리거위치, 뷰포트 트리거 위치
       end: "+=500", //트리거가 끝날때까지의 길이(px)
-      // markers: true,
+        
     },
   });
   gsap.to(".hand_3", {
@@ -72,6 +76,9 @@ gsap.to("#pintest", {
       start: 'center center', //앞에 값은 요소 내부에 위치시킬 트리거 시작지점, 뒤에 값은 화면 자체의 트리거 시작지점
       end: 'top top', //앞에 값은 요소 내부에 위치시킬 트리거 종료지점, 뒤에 값은 화면 자체의 트리거 종료지점
       scrub: 1, //수치를 통해서 원복 방식의 부드러움을 가중시킬 수 있음(true 사용 가능)
+      onEnter: () => {
+        $('.bgm')[0].play();
+      },
       //        markers: true,
     //   onEnter: () => $('.xmove').addClass('bg-danger'),
       //뷰포트에 xmove가 나타날 경우 특정요소를 추가
@@ -88,13 +95,13 @@ gsap.to(".xmove2", {
     start: 'center bottom', //앞에 값은 요소 내부에 위치시킬 트리거 시작지점, 뒤에 값은 화면 자체의 트리거 시작지점
     end: 'top top', //앞에 값은 요소 내부에 위치시킬 트리거 종료지점, 뒤에 값은 화면 자체의 트리거 종료지점
     scrub: 1, //수치를 통해서 원복 방식의 부드러움을 가중시킬 수 있음(true 사용 가능)
-    //        markers: true,
+           markers: true,
     onEnter: () => {
     $('.xmove2').addClass('typewriter1-0')
-    $('.typing')[0].play();
+    $('#typing')[0].play();
   },
   onLeave: () => {
-    $('.typing')[0].pause();
+    $('#typing')[0].pause();
   },
     //뷰포트에 xmove가 나타날 경우 특정요소를 추가
     // onLeave: () => $('.xmove2').removeClass('typewriter1-1'),
@@ -111,10 +118,10 @@ gsap.to(".xmove3", {
     //        markers: true,
     onEnter: () => {
       $('.xmove3').addClass('typewriter1-1')
-      $('.typing')[0].play();
+      $('#typing')[0].play();
     },
     onLeave: () => {
-      $('.typing')[0].pause();
+      $('#typing')[0].pause();
     },
     //뷰포트에 xmove가 나타날 경우 특정요소를 추가
     // onLeave: () => $('.xmove2').removeClass('typewriter1-1'),
@@ -129,7 +136,33 @@ gsap.to(".xmove4", {
     end: 'top top', //앞에 값은 요소 내부에 위치시킬 트리거 종료지점, 뒤에 값은 화면 자체의 트리거 종료지점
     scrub: 1, //수치를 통해서 원복 방식의 부드러움을 가중시킬 수 있음(true 사용 가능)
     //        markers: true,
-    onEnter: () => $('.xmove4').addClass('typewriter1-1'),
+    onEnter: () => {
+      $('.xmove4').addClass('typewriter1-1')
+      $('#typing')[0].play();
+    },
+    onLeave: () => {
+      $('#typing')[0].pause();
+    },
+    //뷰포트에 xmove가 나타날 경우 특정요소를 추가
+    // onLeave: () => $('.xmove2').removeClass('typewriter1-1'),
+    //뷰포트에 xmove가 사라질 경우 특정요소를 삭제
+  },
+});
+gsap.to(".xmove4-2", {
+  duration: 4,
+  scrollTrigger: {
+    trigger: '.xmove4-2',
+    start: 'center bottom', //앞에 값은 요소 내부에 위치시킬 트리거 시작지점, 뒤에 값은 화면 자체의 트리거 시작지점
+    end: 'top top', //앞에 값은 요소 내부에 위치시킬 트리거 종료지점, 뒤에 값은 화면 자체의 트리거 종료지점
+    scrub: 1, //수치를 통해서 원복 방식의 부드러움을 가중시킬 수 있음(true 사용 가능)
+    //        markers: true,
+    onEnter: () => {
+      $('.xmove4-2').addClass('typewriter1-1')
+      $('#typing')[0].play();
+    },
+    onLeave: () => {
+      $('#typing')[0].pause();
+    },
     //뷰포트에 xmove가 나타날 경우 특정요소를 추가
     // onLeave: () => $('.xmove2').removeClass('typewriter1-1'),
     //뷰포트에 xmove가 사라질 경우 특정요소를 삭제
@@ -178,7 +211,7 @@ gsap.to(".xmove7", {
           //  markers: true,
     onEnter: () => {
       $('#my-video')[0].play();
-      $('.bgm')[0].pause();
+      // $('.bgm')[0].pause();
       setTimeout(function() {
         $('body').empty();
         $('body').append( '<div class="end-title opacity-0">END</div>' );
